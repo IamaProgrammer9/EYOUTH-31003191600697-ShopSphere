5c5a64b13984ee61afcd7d5cb91d0e31bc327e21
Omar Mohamed <HelloIamauser@hotmail.com>
2026-08-27 20:45:33 +0300

    remove review service2

--- packages/reviews_service/src/server.ts
+++
@@ -1,7 +0,0 @@
-import app from './index.js';
-
-const port = process.env.PORT || 5200;
-
-app.listen(port, () => {
-    console.log(`Reviews service server started on url http://localhost:${port}`);
-});
